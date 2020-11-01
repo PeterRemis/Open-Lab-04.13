928f26cb0091256478beb753950966b4a899b9b3
PeterRemis <remis@spsknm.sk>
2020-11-01 17:55:27 +0100

    add alg. for searching characters in word

--- Open-Lab-04.13/StringTools.cs
+++ Open-Lab-04.13/StringTools.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.ConstrainedExecution;
 
 namespace Open_Lab_04._13
 {
@@ -6,7 +7,36 @@ namespace Open_Lab_04._13
     {
         public string GetLongestCommonSequence(string str1, string str2)
         {
-            throw new NotImplementedException();
+
+            int dlzka = str1.Length;
+            string str = "";
+
+            for (int j = 0; j < dlzka; j++)
+            {
+                int pocetZhod = 0;
+                string tmpStr = str1[j].ToString();
+                
+                for (int i = j; i < dlzka; i++)
+                {
+                    if (str2.Contains(tmpStr))
+                    {
+                        pocetZhod++;
+                        if (pocetZhod > str.Length)
+                            str = tmpStr;
+
+                        if (i + 1 < dlzka)
+                            tmpStr += str1[i + 1].ToString();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+
+            }
+
+            return str;
         }
     }
 }
